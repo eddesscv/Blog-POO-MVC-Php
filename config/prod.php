a4605110fe799f0b271599f6f6010612c6a3f621
Ednilson MENDES <ednilsonmendes@outlook.com>
2021-11-21 12:19:58 +0100

    favicon

--- config/prod.php
+++ config/prod.php
@@ -1,14 +1,9 @@
 <?php
-/*
 
-//Less constantes
+//Les constantes
 const HOST = 'localhost';
 const DB_NAME = 'blog';
 const CHARSET = 'utf8';
 const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'root';
 const DB_PASS = '';
-
-//'avantage ici est d'avoir une configuration pour la phase de développement. Vous pouvez aussi créer un fichier prod.php dans le dossier config avec les mêmes constantes contenant vos paramètres de dev.
-
-*/
